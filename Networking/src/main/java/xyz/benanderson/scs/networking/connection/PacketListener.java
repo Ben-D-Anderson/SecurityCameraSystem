29bc4c2f1e1ee31f32f5001ff1f35cf019ae02c8
Ben Anderson <benanderson.david@gmail.com>
2022-09-23 14:48:40 +0100

    Implemented PacketListener.java - requires additional documentation and testing

--- Networking/src/main/java/xyz/benanderson/scs/networking/connection/PacketListener.java
+++ Networking/src/main/java/xyz/benanderson/scs/networking/connection/PacketListener.java
@@ -1,9 +1,97 @@
 package xyz.benanderson.scs.networking.connection;
 
-public class PacketListener {
+import xyz.benanderson.scs.networking.Packet;
 
+import java.io.IOException;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Consumer;
+
+public class PacketListener implements AutoCloseable {
+
+    //encapsulated map data structure storing a list of callback code blocks to run for each
+    //packet type.
+    private final Map<Class<? extends Packet>, List<Consumer<Packet>>> callbacks;
+    //encapsulated asynchronous thread, on which, packets are listened for.
+    private final Thread packetListeningThread;
+    //private, thread-safe, atomic boolean variable to control the condition-controlled
+    //loop in the packet listening thread.
+    private final AtomicBoolean listeningForPackets = new AtomicBoolean(true);
+
+    /**
+     * Constructor for {@code PacketListener} class
+     *
+     * @param connection {@code Connection} object that this {@code PacketListener} is
+     *                                     listening for packets from.
+     */
     public PacketListener(Connection connection) {
+        //initialise the callback map with an empty hashmap
+        this.callbacks = new HashMap<>();
 
+        //create thread
+        this.packetListeningThread = new Thread(() -> {
+            //code to run in the thread
+            while (listeningForPackets.get() && connection.isConnected()) {
+                //try to read the packet from the underlying PacketController
+                try {
+                    Packet packet = connection.getPacketController().readPacketFromSocket();
+                    //if the packet was successfully read from the connection,
+                    //run callbacks associated with the packet
+                    runCallbacks(packet);
+                } catch (IOException e) {
+                    //exception will be thrown if the connection was closed, in which
+                    //case ignore it and return
+                    if (!connection.isConnected()) return;
+
+                    //if the packet was NOT successfully read from the connection,
+                    //log the error to the standard error stream.
+                    System.err.println("[ERROR] An error occurred whilst reading a packet" +
+                            " from a connection:");
+                    e.printStackTrace();
+                } catch (ClassNotFoundException e) {
+                    //if the packet received was not a valid packet type, log the error
+                    //to the standard error stream.
+                    System.err.println("[WARNING] An unknown packet type was received" +
+                            " from a connection: ");
+                    e.printStackTrace();
+                }
+            }
+        }, "Packet Listening Thread" /* name of the thread */);
+
+        //start the asynchronous packet listening thread
+        this.packetListeningThread.start();
+    }
+
+    public <T extends Packet> void addCallback(Class<T> packetClass, Consumer<T> callback) {
+        //if a callback of the packet class type has not already been registered,
+        //then add it to the map with a new, empty linked list.
+        if (!callbacks.containsKey(packetClass))
+            callbacks.put(packetClass, new LinkedList<>());
+
+        //add the callback to the list corresponding to the packet class
+        //key in the callbacks map
+        callbacks.get(packetClass).add((Consumer<Packet>) callback);
+    }
+
+    private void runCallbacks(Packet packet) {
+        if (callbacks.containsKey(packet.getType()))
+            callbacks.get(packet.getType()).forEach(callback -> callback.accept(packet));
+    }
+
+    /**
+     * Method overriding the default `close()` implementation from {@link AutoCloseable}.
+     * This method disables the constant listening for packets on the asynchronous
+     * 'Packet Listening Thread' and waits for the thread to die.
+     *
+     * @throws InterruptedException thrown if an exception occurs when waiting for the thread to die.
+     */
+    @Override
+    public void close() throws InterruptedException {
+        //set loop-controlling variable to false in order to disable the while loop
+        //in the packet listening thread
+        this.listeningForPackets.set(false);
+        //wait for the packet listening thread to die
+        this.packetListeningThread.join();
     }
 
 }
