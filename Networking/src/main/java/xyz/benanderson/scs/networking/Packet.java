4e9805e269641c42edaad95e74a82ea41e087802
Ben Anderson <benanderson.david@gmail.com>
2022-10-05 22:47:00 +0100

    Importing Serializable

--- Networking/src/main/java/xyz/benanderson/scs/networking/Packet.java
+++ Networking/src/main/java/xyz/benanderson/scs/networking/Packet.java
@@ -3,8 +3,10 @@ package xyz.benanderson.scs.networking;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @AllArgsConstructor
-public abstract class Packet {
+public abstract class Packet implements Serializable {
 
     @Getter
     private final Class<? extends Packet> type;
