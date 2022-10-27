3f2aa22800df9e6145c1b0fa48ae4b2fc76fcd0c
Ben Anderson <benanderson.david@gmail.com>
2022-10-27 20:42:39 +0100

    added documentation & comments, + minor adjustments

--- Server/src/main/java/xyz/benanderson/scs/server/networking/Server.java
+++ Server/src/main/java/xyz/benanderson/scs/server/networking/Server.java
@@ -18,6 +18,7 @@ import java.util.function.Consumer;
 
 public class Server implements AutoCloseable {
 
+    //attributes of the server
     @Getter
     private ServerSocket serverSocket;
     private Thread receiveConnectionsThread;
@@ -30,18 +31,39 @@ public class Server implements AutoCloseable {
     private final BiConsumer<Connection, Server> clientDisconnectListener;
     private final Consumer<Server> serverShutdownListener;
 
+    /**
+     * Constructor with package-private (default) visibility to allow only the {@code ServerBuilder}
+     * class to instantiate {@code Server} instances.
+     *
+     * @param serverBuilder builder to construct this {@code Server} from
+     */
     Server(ServerBuilder serverBuilder) {
+        //create a synchronized map to avoid race conditions in a multithreaded environment
+        //uses a TreeMap as a key-value store for connections, where the key is the
+        //Connection's identifier and the value is the Connection object.
+        //A TreeMap was used instead of a HashMap as O(1) retrieval is not essential as
+        //operations against this map will not occur often.
         this.connections = Collections.synchronizedSortedMap(new TreeMap<>());
+        //mark the server as running
         this.running = new AtomicBoolean(true);
+        //assign attributes from serverBuilder...
         this.port = serverBuilder.getPort();
         this.bindAddress = serverBuilder.getBindAddress();
         this.clientConnectListener = serverBuilder.getClientConnectListener();
-        this.clientDisconnectListener = serverBuilder.getClientDisconnectListener();
+        //append code to remove the Connection from the Server's internal connections map
+        //to the end of the client disconnect listener
+        BiConsumer<Connection, Server> closeListener = (con, server) -> {
+            try { connections.remove(con.getId()).close(); } catch (Exception ignored) {}
+        };
+        this.clientDisconnectListener = serverBuilder.getClientDisconnectListener().andThen(closeListener);
         this.serverShutdownListener = serverBuilder.getServerShutdownListener();
     }
 
+    /**
+     * @return boolean denoting if the {@code Server} is open and running.
+     */
     public boolean isOpen() {
-        return serverSocket != null && !serverSocket.isClosed();
+        return serverSocket != null && !serverSocket.isClosed() && running.get();
     }
 
     /**
@@ -51,48 +73,74 @@ public class Server implements AutoCloseable {
         return Collections.unmodifiableSortedMap(connections);
     }
 
+    /**
+     * Starts the {@code Server}'s receive-connections thread and opens a {@code ServerSocket}
+     * to allow connections.
+     */
     public void start() throws IOException {
         start(new ServerSocket(this.port, 5, this.bindAddress));
     }
 
-    public void start(ServerSocket serverSocket) {
+    //private-visibility method used by the public visibility start() method
+    private void start(ServerSocket serverSocket) {
         this.serverSocket = serverSocket;
         this.receiveConnectionsThread = new Thread(this::receiveConnections);
         this.receiveConnectionsThread.start();
     }
 
+    /**
+     * Closes the {@code Server} and stops listening for new connections. The server shutdown
+     * listener will be triggered by the receive-connections thread. All connections will be
+     * closed and cleared from the {@code Server}.
+     */
     @Override
-    public void close() throws Exception {
+    public void close() {
+        //attempt to stop listening for connections on the receive-connections thread
         running.set(false);
-        serverSocket.close();
-        receiveConnectionsThread.join();
+        try {
+            serverSocket.close();
+        } catch (Exception ignored) {}
+        //wait for the receive-connections thread to terminate (also waits for the
+        //server shutdown listener to execute).
+        try {
+            receiveConnectionsThread.join();
+        } catch (InterruptedException ignored) {}
+        //closes all connections
         connections.values().forEach(con -> {
             try {
                 con.close();
             } catch (Exception ignored) {}
         });
+        //clear connections from memory of the server
         connections.clear();
     }
 
+    //private-visibility method ran only by the receive-connections thread
     private void receiveConnections() {
-        while (running.get() && !serverSocket.isClosed()) {
+        //condition-controlled loop to keep listening thread active as long as the
+        //server is running and open
+        while (isOpen()) {
+            //don't accept new connections if already at max connections as specified in config
             if (connections.size() >= ConfigurationWrapper.getInstance().getMaxConnections())
                 continue;
             try {
+                //block until connection is accepted from the server's socket
                 Socket socket = serverSocket.accept();
+                //instantiate Connection object (from connection framework) using
+                //the connection's socket
                 Connection connection = new Connection(socket);
+                //add connection to server's map of connections
                 connections.put(connection.getId(), connection);
+                //run the client connection listener with the newly accepted connection
                 clientConnectListener.accept(connection, this);
-                BiConsumer<Connection, Server> closeListener = (con, server) -> {
-                    try {
-                        connections.remove(con.getId()).close();
-                    } catch (Exception ignored) {}
-                };
-                connection.setDisconnectListener(con -> clientDisconnectListener.andThen(closeListener).accept(con, this));
+                //add disconnect listener to the connection
+                connection.setDisconnectListener(con -> clientDisconnectListener.accept(con, this));
             } catch (IOException e) {
+                //print error if an exception occurs
                 e.printStackTrace();
             }
         }
+        //run the server shutdown listener as isOpen() now returns false
         serverShutdownListener.accept(this);
     }
 
