e76896dfa5330a515bfaaf9da5d733dd1b56faba
Ben Anderson <benanderson.david@gmail.com>
2022-12-02 14:00:56 +0000

    added Main.java runner class for server software

--- Server/src/main/java/xyz/benanderson/scs/server/Main.java
+++ Server/src/main/java/xyz/benanderson/scs/server/Main.java
@@ -1,9 +1,32 @@
 package xyz.benanderson.scs.server;
 
+import com.github.sarxos.webcam.Webcam;
+import xyz.benanderson.scs.networking.Packet;
+import xyz.benanderson.scs.networking.packets.MediaPacket;
+import xyz.benanderson.scs.server.configuration.ConfigurationWrapper;
+import xyz.benanderson.scs.server.networking.Server;
+import xyz.benanderson.scs.server.networking.ServerBuilder;
+import xyz.benanderson.scs.server.video.CameraViewer;
+
+import java.io.IOException;
+import java.net.InetAddress;
+
 public class Main {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        ServerBuilder serverBuilder = new ServerBuilder(ConfigurationWrapper.getInstance().getServerPort(),
+                InetAddress.getByName(ConfigurationWrapper.getInstance().getServerAddress()));
+        try (Server server = serverBuilder.build();
+             CameraViewer cameraViewer = new CameraViewer(Webcam.getDefault())) {
+            while (true) {
+                cameraViewer.captureImage().ifPresent(img -> {
+                    Packet packet = new MediaPacket(img);
+                    server.getConnections().values().forEach(conn -> {
+                        conn.getPacketSender().sendPacket(packet);
+                    });
+                });
+            }
+        }
     }
 
 }
